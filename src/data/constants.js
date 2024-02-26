7cbea2d71a4a78fbcf232ff5a90b009ea3029e14
Keerthy Baby <keerthybaby025@gmail.com>
2024-02-26 21:50:28 +0530

    Add HeroSection component

---
+++ src/data/constants.js
@@ -0,0 +1,14 @@
+export const Bio = {
+  name: "Keerthy Baby",
+  roles: [
+    "Full Stack Developer",
+    "Programmer",
+  ],
+  description:
+    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+  github: "https://github.com/Keerthybaby",
+  resume:
+    "",
+  linkedin: "https://www.linkedin.com/in/keerthy-baby-56b176222/",
+};
+
